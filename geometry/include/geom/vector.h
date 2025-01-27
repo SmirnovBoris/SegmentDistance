3ce434f461ccb17108d85a1672a23b67e7225e2e
Boris Smirnov <SmirnofBoris@gmail.com>
2025-01-27 19:44:27 +0100

    Cross product

--- geometry/include/geom/vector.h
+++ geometry/include/geom/vector.h
@@ -18,6 +18,9 @@ public:
         , z(z)
     {}
 
+    static Vector_3D zero() {
+        return Vector_3D{ 0., 0., 0. };
+    }
    
     const scalar_type& get_x() const { return x; }
     const scalar_type& get_y() const { return y; }
@@ -37,6 +40,8 @@ public:
 
 private:
     scalar_type x, y, z;
+
+    friend Vector_3D<scalar_type> cross_product<>(const Vector_3D<scalar_type>& a, const Vector_3D<scalar_type>& b);
 }; 
 
 template<class scalar_type>
@@ -52,6 +57,15 @@ bool vector_eq(const Vector_3D<scalar_type>& l, const Vector_3D<scalar_type>& r,
     return eq(l.get_x(), r.get_x()) && 
            eq(l.get_y(), r.get_y()) &&
            eq(l.get_z(), r.get_z());
+}
+
+template<class scalar_type>
+Vector_3D<scalar_type> cross_product(const Vector_3D<scalar_type>& a, const Vector_3D<scalar_type>& b) {
+    return Vector_3D<scalar_type>{ 
+        a.y * b.z - a.z * b.y,
+        a.x * b.z - a.z * b.x,
+        a.x * b.y - a.y * b.x  
+    };
 }
 
 } // namespace geom
--- test/helloworld.cpp
+++ test/helloworld.cpp
@@ -22,7 +22,7 @@ private:
 public:
     static constexpr scalar_type eps = eps_value();
 
-    static void expect_vector_eq(const vector& l, vector& r) {
+    static void expect_vector_eq(const vector& l, const vector& r) {
         EXPECT_NEAR(l.get_x(), r.get_x(), eps);
         EXPECT_NEAR(l.get_y(), r.get_y(), eps);
         EXPECT_NEAR(l.get_z(), r.get_z(), eps);
@@ -34,29 +34,45 @@ TYPED_TEST_SUITE(GeomTest, MyTypes);
 
 
 TYPED_TEST(GeomTest, Basics) {
-    geom::Point_3D<float> p{ 1., 2., 3. };
+    using point =  typename TestFixture::point; 
+    using vector =  typename TestFixture::vector; 
+    using scalar_type =  typename TestFixture::scalar_type; 
+    point p{ 1., 2., 3. };
     EXPECT_NEAR(1.f, p.get_x(), TestFixture::eps);
     EXPECT_NEAR(2.f, p.get_y(), TestFixture::eps);
     EXPECT_NEAR(3.f, p.get_z(), TestFixture::eps);
 
-    geom::Vector_3D<float> v{ 10., -20., 30. };
+    vector v{ 10., -20., 30. };
     EXPECT_NEAR(10.f, v.get_x(), TestFixture::eps);
     EXPECT_NEAR(-20.f, v.get_y(), TestFixture::eps);
     EXPECT_NEAR(30.f, v.get_z(), TestFixture::eps);
 
-    typename TestFixture::scalar_type length = 37.416573867739;
+    scalar_type length = 37.416573867739;
     EXPECT_NEAR(length, v.dist(), TestFixture::eps);
     EXPECT_NEAR(std::pow(length, 2.), v.dist2(), TestFixture::eps);
 }
 
 
 TYPED_TEST(GeomTest, Vector) {
-    typename TestFixture::vector a{ 0., 1., 2. };
-    typename TestFixture::vector b{ 0.f + TestFixture::eps / 2.f, 1.f - TestFixture::eps / 2.f, 2.f };
-    typename TestFixture::vector c{ 100., 1., 2. };
+    using vector =  typename TestFixture::vector; 
+    using scalar_type =  typename TestFixture::scalar_type; 
+    vector a{ 0., 1., 2. };
+    vector b{ 0.f + TestFixture::eps / 2.f, 1.f - TestFixture::eps / 2.f, 2.f };
+    vector c{ 100., 1., 2. };
 
     TestFixture::expect_vector_eq(a, b);
-    EXPECT_TRUE(geom::vector_eq<typename TestFixture::scalar_type>(a, b, TestFixture::eps));
-    EXPECT_FALSE(geom::vector_eq<typename TestFixture::scalar_type>(a, c, TestFixture::eps));
-    EXPECT_FALSE(geom::vector_eq<typename TestFixture::scalar_type>(b, c, TestFixture::eps));
+    EXPECT_TRUE(geom::vector_eq<scalar_type>(a, b, TestFixture::eps));
+    EXPECT_FALSE(geom::vector_eq<scalar_type>(a, c, TestFixture::eps));
+    EXPECT_FALSE(geom::vector_eq<scalar_type>(b, c, TestFixture::eps));
+}
+
+
+TYPED_TEST(GeomTest, CrossProduct) {
+    using vector =  typename TestFixture::vector; 
+    vector ox{ 1., 0., 0. };
+    vector oy{ 0., 1., 0. };
+    vector oz{ 0., 0., 1. };
+
+    TestFixture::expect_vector_eq(geom::cross_product(ox, oy), oz);
+    TestFixture::expect_vector_eq(geom::cross_product(ox, ox), vector::zero());
 }
